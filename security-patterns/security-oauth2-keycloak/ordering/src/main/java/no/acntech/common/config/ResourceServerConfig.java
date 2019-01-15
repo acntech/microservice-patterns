31eb92e59491dbafaf787d6dda42c6d94fea667a
Teo <onlyteo@live.com>
2019-01-15 22:06:46 +0100

    Added security example with KeyCloak server

--- security-patterns/security-oauth2-keycloak/ordering/src/main/java/no/acntech/common/config/ResourceServerConfig.java
+++ security-patterns/security-oauth2-keycloak/ordering/src/main/java/no/acntech/common/config/ResourceServerConfig.java
@@ -10,11 +10,12 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 @Configuration
 public class ResourceServerConfig {
 
+    @SuppressWarnings("Duplicates")
     @Primary
     @Bean
     public RemoteTokenServices tokenService() {
         RemoteTokenServices tokenService = new RemoteTokenServices();
-        tokenService.setCheckTokenEndpointUrl("http://localhost:8000/oauth/check_token");
+        tokenService.setCheckTokenEndpointUrl("http://localhost:8000/auth/realms/master/protocol/openid-connect/token");
         tokenService.setClientId("ordering");
         tokenService.setClientSecret("secret");
         return tokenService;
