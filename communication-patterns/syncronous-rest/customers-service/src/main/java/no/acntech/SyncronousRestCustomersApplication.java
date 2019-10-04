f68717647f35da9fedec0bc83e89176e47bdc93c
Ørjan Johansen <orjan.johansen@accenture.com>
2019-10-04 10:53:08 +0200

    Customer rest application

--- communication-patterns/syncronous-rest/customers-service/src/main/java/no/acntech/SyncronousRestCustomersApplication.java
+++ communication-patterns/syncronous-rest/customers-service/src/main/java/no/acntech/SyncronousRestCustomersApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventNotificationCustomersApplication extends SpringBootServletInitializer {
+public class SyncronousRestCustomersApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventNotificationCustomersApplication.class, args);
+        SpringApplication.run(SyncronousRestCustomersApplication.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EventNotificationCustomersApplication.class);
+        return application.sources(SyncronousRestCustomersApplication.class);
     }
 }
