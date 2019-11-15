7fc03d7f47c77d628957f8d74d191c8e301662cf
Ørjan Johansen <orjan.johansen@accenture.com>
2019-11-15 13:17:54 +0100

    ordering and warehouse application

---
+++ communication-patterns/event-sourcing/warehouse-service/src/main/java/no/acntech/EventSourcingWarehouseApplication.java
@@ -0,0 +1,20 @@
+package no.acntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EventSourcingWarehouseApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingWarehouseApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventSourcingWarehouseApplication.class);
+    }
+
+}
