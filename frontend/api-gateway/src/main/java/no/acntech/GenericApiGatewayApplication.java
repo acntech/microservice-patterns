4c932b7313cd4da4f3d4e62f1a81e17bec4d70f8
Teo <onlyteo@live.com>
2022-11-24 22:17:57 +0100

    Refactored frontend structure, migrated tracing example

---
+++ frontend/api-gateway/src/main/java/no/acntech/GenericApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package no.acntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericApiGatewayApplication.class, args);
+    }
+}
---
+++ frontend/portal/README.md
@@ -0,0 +1,17 @@
+# Portal
+
+This module is a React/Redux based UI for use with the different examples.
+
+## Usage
+
+1. Install NPM dependencies.
+
+Yarn: `yarn install`
+
+NPM: `npm install`
+
+2. Start Webpack dev server
+
+Yarn: `yarn start`
+
+NPM: `npm run start`
